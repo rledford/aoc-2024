5aeacf90e45295f009eb9dfc95873dc47f9b6702
rledford <ryan.c.ledford@gmail.com>
2024-12-03 21:24:22 -0600

    Day 3 solution part 2.

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,97 +1,86 @@
 use std::env;
 use std::fs::read_to_string;
 
-const INSTR: &str = "mul";
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = get_input(args.get(1));
 
-    let mut slice: &str; // moving window slice
+    let mut slice: &str;
     let mut cursor: usize = 0;
     let mut answer_1: usize = 0;
+    let mut answer_2: usize = 0;
+    let mut is_mul_enabled = true;
 
     loop {
         slice = &input[cursor..];
-        println!("{slice}");
+        if slice.starts_with("m") {
+            let (v, cur) = try_mul(slice);
+            if let Some(result) = v {
+                answer_1 += result;
+                if is_mul_enabled {
+                    answer_2 += result;
+                }
+            }
 
-        if let Some(cur) = next_instruction_predicate(INSTR, slice) {
-            println!("next cursor position {cur}");
+            cursor += cur;
+        } else if slice.starts_with("d") {
+            let (v, cur) = try_do_dont(slice);
+            if let Some(result) = v {
+                is_mul_enabled = result;
+            }
+
             cursor += cur;
         } else {
-            println!("no more instructions");
+            cursor += 1;
+        }
+
+        if cursor >= input.len() {
             break;
         }
-
-        slice = &input[cursor..];
-
-        let (operands, cur) = get_instruction_operands(slice);
-
-        if let Some((a, b)) = operands {
-            answer_1 += a * b;
-        }
-
-        cursor += cur;
     }
 
     println!("part 1 answer: {answer_1}");
+    println!("part 2 answer: {answer_2}");
 }
 
-/// Returns the index of the first character that follows "mul" in the provided slice
-fn next_instruction_predicate(instruction: &str, slice: &str) -> Option<usize> {
-    if slice.len() < 3 {
-        return None;
-    }
-
-    let mut cmp: &str;
-    let offset = instruction.len();
-
-    for i in 0..(slice.len() - offset) {
-        cmp = &slice[i..(i + offset)];
-        if cmp.eq(instruction) {
-            return Some(i + offset);
-        }
-    }
-
-    None
-}
-
-/// Returns values extracted from the provided slice that appear within "(" and ")"
-/// and an index offset of the last successfully parsed character. If the search was
-/// stopped due to no matching sequence found, e.g. "(12,mul)" then the returned index
-/// offset will be the position in the slice that the match logic stopped.
-///
-/// Example: "(12,mul)" would return (None, 4) since 4 is the index of the "m"
-/// because the "m" was not an expected character of the operand sequence, indicating
-/// that the search for the next "mul" should start at index 4 (relative to the slice)
-fn get_instruction_operands(slice: &str) -> (Option<(usize, usize)>, usize) {
-    println!("getting operands");
-    if slice.len() < 5 || !slice.starts_with("(") {
-        println!("input not long enough");
-        return (None, 0);
+// Returns (Some(product), cursor_offset) when mul(a, b) found
+// Returns (None, 1) when mul(a, b) not found
+fn try_mul(slice: &str) -> (Option<usize>, usize) {
+    if !slice.starts_with("mul(") {
+        return (None, 1);
     }
 
     if let Some(closing_index) = slice.find(")") {
-        println!("has cloing parenthases at {closing_index}");
-        let segment = &slice[1..closing_index];
+        let segment = &slice[4..closing_index];
         let parts: Vec<&str> = segment.split(",").collect();
 
-        dbg!(&parts);
-
         if parts.len() != 2 {
-            return (None, 0);
+            return (None, 1);
         }
 
         if let Some(a) = parts.get(0).unwrap().parse::<usize>().ok() {
             if let Some(b) = parts.get(1).unwrap().parse::<usize>().ok() {
-                return (Some((a, b)), segment.len());
+                return (Some(a * b), closing_index);
             }
         }
-    } else {
-        println!("no closing parenthases");
     }
 
-    (None, 0)
+    (None, 1)
+}
+
+/// Returns (Some(true), cursor_offset) if do() is found
+/// Returns (Some(false), cursor_offset) if don't() is found
+/// Returns (None, 1) otherwise
+fn try_do_dont(slice: &str) -> (Option<bool>, usize) {
+    if slice.starts_with("do()") {
+        return (Some(true), 4);
+    }
+
+    if slice.starts_with("don't()") {
+        return (Some(false), 7);
+    }
+
+    (None, 1)
 }
 
 fn get_input(path: Option<&String>) -> String {
